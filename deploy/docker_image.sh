fc797c951efc52703c9ef593a96781a552e6e64c
Richard <quanlongwang@gmail.com>
2024-08-11 19:06:08 +0800

    Update docker_image.sh

--- deploy/docker_image.sh
+++ deploy/docker_image.sh
@@ -3,7 +3,7 @@
 tag=$1
 IMAGE_NAME="user-service"
 NAMESPACE="go-microservices"
-REGISTRY="registry.cn-shanghai.aliyuncs.com"
+REGISTRY="registry.cn-hangzhou.aliyuncs.com"
 
 if [[ -z "$tag" ]]; then
   echo "tag is empty"
@@ -13,14 +13,10 @@ fi
 
 # build image
 echo "1. build docker image"
-docker build -t ${REGISTRY}/${NAMESPACE}/${IMAGE_NAME}:${tag} -f deploy/docker/Dockerfile .
-
-# docker tag local-image:tagname new-repo:tagname
-echo "2. gen docker tag"
-docker tag ${IMAGE_NAME}:${tag} ${REGISTRY}/${NAMESPACE}/${IMAGE_NAME}:${tag}
+docker build -t ${REGISTRY}/${NAMESPACE}/${IMAGE_NAME}:${tag} -t ${REGISTRY}/${NAMESPACE}/${IMAGE_NAME}:latest -f deploy/docker/Dockerfile .
 
 # docker push new-repo:tagname
-echo "3. push docker image to hub"
+echo "2. push docker image to remote hub"
 docker push ${REGISTRY}/${NAMESPACE}/${IMAGE_NAME}:${tag}
 
 echo "Done. push docker image success."
